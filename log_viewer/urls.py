69f6f4898dd415f2451ecb1e5cbb50a9edc23f61
ksshin <ksshin@lunit.io>
2020-08-31 17:02:11 +0900

    Edit: URL regex patterns

--- log_viewer/urls.py
+++ log_viewer/urls.py
@@ -10,14 +10,14 @@ app_name = 'log_viewer'
 if django.VERSION >= (2, 0):
     from django.urls import re_path
     urlpatterns = [
-        re_path(r'^json/(?P<file_name>[\.\w-]*)/(?P<page>[0-9]+)', log_json, name='log_json_view'),
-        re_path(r'^json/(?P<file_name>[\.\w-]*)', log_json, name='log_json_view'),
-        re_path(r'^', log_viewer, name='log_file_view'),
+        re_path(r'^json/(?P<file_name>[\.\w-]*)/(?P<page>[0-9]+)/?$', log_json, name='log_json_view'),
+        re_path(r'^json/(?P<file_name>[\.\w-]*)/?$', log_json, name='log_json_view'),
+        re_path(r'^/?$', log_viewer, name='log_file_view'),
     ]
 else:
     from django.conf.urls import url
     urlpatterns = [
-        url(r'^json/(?P<file_name>[\.\w-]*)/(?P<page>[0-9]+)', log_json, name='log_json_view'),
-        url(r'^json/(?P<file_name>[\.\w-]*)', log_json, name='log_json_view'),
-        url(r'^', log_viewer, name='log_file_view'),
+        url(r'^json/(?P<file_name>[\.\w-]*)/(?P<page>[0-9]+)/?$', log_json, name='log_json_view'),
+        url(r'^json/(?P<file_name>[\.\w-]*)/?$', log_json, name='log_json_view'),
+        url(r'^/?$', log_viewer, name='log_file_view'),
     ]
